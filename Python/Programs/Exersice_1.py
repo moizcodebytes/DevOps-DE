cf1ea1f8de30d99db0a1c8a4df9e7214bd6891ca
Mohammed Moizuddin Ahmed <moizuddin91@gmail.com>
2025-07-30 22:19:03 +0530

    Update Exersice_1.py

--- Python/Programs/Exersice_1.py
+++ Python/Programs/Exersice_1.py
@@ -3,6 +3,11 @@
 ## Declaring variables and values
 x = 500
 y = 300
+
+## Taking numbers and operators from user 
+x = int(input("please enter the value1 :" ))
+y = int(input("please enter the value2 :"))
+
 sum = x + y
 print("the sum of two numbers is " ,sum)
 sub = x - y
@@ -11,3 +16,13 @@ product = x * y
 print("the product of two numbers is " ,product)
 division = x / y
 print("the division of two numbers is " ,division)
+
+### output
+
+''' please enter the value1 :5
+please enter the value2 :10
+the sum of two numbers is  15
+the sub of two numbers is  -5
+the product of two numbers is  50
+the division of two numbers is  0.5
+'''
