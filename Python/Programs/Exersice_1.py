c265b91b6d914b82b09031100dcdf41bc9dccb63
Mohammed Moizuddin Ahmed <moizuddin91@gmail.com>
2025-07-30 22:07:10 +0530

    Update Exersice_1.py

--- Python/Programs/Exersice_1.py
+++ Python/Programs/Exersice_1.py
@@ -1,1 +1,13 @@
-print("hello, World")
+## Create a calculator which performs add,sub,multiply,division of two numbers.
+
+## Declaring variables and values
+x = 500
+y = 300
+sum = x + y
+print("the sum of two numbers is " ,sum)
+sub = x - y
+print("the sub of two numbers is " ,sub)
+product = x * y
+print("the product of two numbers is " ,product)
+division = x / y
+print("the division of two numbers is " ,division)
